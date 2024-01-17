140c7654dcc58461d8335b94e0188b46af1cdacd
Johnnielp <ioannislappas1992@gmail.com>
2024-01-17 15:53:33 +0200

    corrected typo

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -46,7 +46,7 @@
                     <p>
                         If you no longer wish to track a city, simply select
                         the city within the home page. At the bottom of the
-                        page, there will be am option to delete the city.
+                        page, there will be an option to delete the city.
                     </p>
                 </div>
             </BaseModal>
